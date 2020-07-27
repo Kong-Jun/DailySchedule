8a009d354d6ab16574054ffeaa83540da18fbc0d
Kong-Jun <kongjun18@outlook.com>
2020-07-27 15:52:14 +0800

    添加题目

--- practice/拼数/src/main.rs
+++ practice/拼数/src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::cmp::PartialOrd;
 fn main() {
     let mut n = String::new();
     io::stdin().read_line(&mut n).unwrap();
@@ -11,11 +10,76 @@ fn main() {
         print!("{}", buf.trim().parse::<u128>().unwrap());
         return ();
     }
-    let mut vec: Vec<&str> = buf.trim().split_whitespace().collect();
+    let mut vec: Vec<String> = buf.trim().split_whitespace().map(|str| str.to_owned()).collect();
     
-    vec.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    // 傻了，居然忘记了可以直接比较大小。
+    // vec.sort_by(|a, b| {
+    //     if a.len() == b.len() {
+    //         b.partial_cmp(a).unwrap()
+    //     } else if a.len() > b.len() {
+    //         let a_bytes = a.bytes().collect::<Vec<u8>>();
+    //         let b_bytes = b.bytes().collect::<Vec<u8>>();
+    //         match a_bytes[b.len()].cmp(&a_bytes[0]) {
+    //             Ordering::Less => Ordering::Greater,
+    //             Ordering::Greater => Ordering::Less,
+    //             Ordering::Equal => {
+    //                 match a_bytes[1].cmp(&a_bytes[0]) {
+    //                     Ordering::Less => Ordering::Less,
+    //                     Ordering::Greater => Ordering::Greater,
+    //                     Ordering::Equal => {
+    //                         match a_bytes[a.len() - 1].cmp(&b_bytes[b.len() - 1]) {
+    //                             Ordering::Less => Ordering::Less,
+    //                             Ordering::Greater => Ordering::Greater,
+    //                             Ordering::Equal => Ordering::Equal,
+    //                         }
+    //                     }
+                        
+    //                 }
+    //             }
+    //         }
+    //     } else {
+    //         let a_bytes = a.bytes().collect::<Vec<u8>>();
+    //         let b_bytes = b.bytes().collect::<Vec<u8>>();
+    //         match b_bytes[a.len()].cmp(&b_bytes[0]) {
+    //             Ordering::Greater => Ordering::Less,
+    //             Ordering::Less => Ordering::Greater,
+    //             Ordering::Equal => {
+    //                 match b_bytes[1].cmp(&b_bytes[0]) {
+    //                     Ordering::Less => Ordering::Less,
+    //                     Ordering::Greater => Ordering::Greater,
+    //                     Ordering::Equal => {
+    //                         match a_bytes[a.len() - 1].cmp(&b_bytes[b.len() - 1]) {
+    //                             Ordering::Less => Ordering::Less,
+    //                             Ordering::Greater => Ordering::Greater,
+    //                             Ordering::Equal => Ordering::Equal,
+    //                         }
+    //                     }
+                        
+    //                 }
+    //             }
+    //         }
+    //     }
+        // else .len() < b.len()){
+            // let a_bytes = a.bytes().collect::<Vec<u8>>();
+            // match a_bytes[0].cmp(a_bytes[a.len()]) {
+            //     Ordering::Less => Ordering 
+            // }
+
+            // }
+        // }
+    // });
     
-    let mut output = String::from(vec[0]);
+    vec.sort_by(|a, b| {
+        if a.len() == b.len() {
+            b.cmp(a)
+        }
+        else {
+            let a_b = a.clone() + b.as_str();
+            let b_a = b.clone() + a.as_str();
+            b_a.cmp(&a_b)
+        }
+    });
+    let mut output = String::from(&vec[0]);
 
     for i in 1..n {
         let str = vec[i].clone();
